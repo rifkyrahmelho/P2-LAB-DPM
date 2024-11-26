ffcdd8b5f8762538aabfbcaf98ea767ce029429a
Apple <apple@Apples-MacBook-Air.local>
2024-11-26 19:48:03 +0800

    Percobaan commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css';  // Import file CSS untuk styling
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {/* Teks di tengah layar */}
+      <div className="center-text">
+        Teks di Tengah Layar
+      </div>
+
+      {/* Kotak 1 dan Kotak 2 */}
+      <div className="flex-container">
+        <div className="box box1"></div>
+        <div className="box box2"></div>
+      </div>
     </div>
   );
 }
